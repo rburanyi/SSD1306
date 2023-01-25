2992067bfd5b808e7f5c78291cf259e9a025c5e2
Robi Buranyi <rburanyi@google.com>
2023-01-24 21:01:31 -0800

    update images
    Signed-off-by: Robi Buranyi <rburanyi@google.com>

--- res/all.sh
+++ res/all.sh
@@ -3,6 +3,6 @@
 set -e
 
 for i in electrical network exclamation ; do
-  convert $i.jpg -depth 1 -resize 48x48 -type bilevel BMP3:$i.bmp
+  convert $i.jpg -depth 1 -resize 48x48 -threshold 50% -type bilevel BMP3:$i.bmp
   bin2c -C $i.c $i.bmp
 done
